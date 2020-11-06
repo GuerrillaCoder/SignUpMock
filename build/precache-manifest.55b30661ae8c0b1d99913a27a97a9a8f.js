eef1c45d8ed4557c09ecccf104bbb20bd2c8cc33
GuerrillaCoder <michael@digitalreach.online>
2020-11-06 20:02:23 +0000

    fix path

---
+++ build/precache-manifest.55b30661ae8c0b1d99913a27a97a9a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1142385f9041bbee421c25a0d3c094a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d4121d7281370555e5a",
+    "url": "./static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "3f684e3add759129abc7",
+    "url": "./static/js/2.696b3c40.chunk.js"
+  },
+  {
+    "revision": "f2f0c53c8055a0289086049d414080ec",
+    "url": "./static/js/2.696b3c40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4121d7281370555e5a",
+    "url": "./static/js/main.54078a0f.chunk.js"
+  },
+  {
+    "revision": "b47587632c7abd5d19f5",
+    "url": "./static/js/runtime-main.0f9568b8.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84ba5a3c8fd9a7fb816f33b076b23c93.js"
+  "./precache-manifest.55b30661ae8c0b1d99913a27a97a9a8f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
